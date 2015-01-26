0f61dbcc83673c6c8ecd29623a4b102b8ef4724f
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2015-01-26 23:16:55 +0100

    unit test for URL mapping, dummy view

--- TMBucket/urls.py
+++ TMBucket/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'TMBucket.views.home', name='home'),
+     url(r'^$', 'translations.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+
 )
