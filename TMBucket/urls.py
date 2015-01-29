3314c647afb6a424302b720f09050842e7cc0f94
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2015-01-29 22:27:24 +0100

    URL added for new forms

--- TMBucket/urls.py
+++ TMBucket/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'translations.views.home_page', name='home'),
      url(r'^tms/new_translation_memory', 'translations.views.view_tm', name='view_tm'),
+     url(r'^tms/new$', 'translations.views.new_tm', name='new_tm'),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
