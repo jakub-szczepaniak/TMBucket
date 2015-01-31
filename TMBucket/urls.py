ecc6c456f2dce6bda92c1eaa13db3e133fbb773d
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2015-01-31 14:24:37 +0100

    two new urls for TM creation

--- TMBucket/urls.py
+++ TMBucket/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'translations.views.home_page', name='home'),
      url(r'^tms/new-translation-memory/$', 'translations.views.view_tms', name='view_tms'),
+     url(r'tms/new$', 'translations.views.new_tm', name='new_tm'),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
