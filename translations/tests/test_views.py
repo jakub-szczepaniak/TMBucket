24bf489066626b0df4cee80ccd471034461987c3
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2015-02-02 21:10:02 +0100

    separated model and views tests

--- translations/tests/test_views.py
+++ translations/tests/test_views.py
@@ -118,47 +118,4 @@ class NewTransUnitTest(TestCase):
             'target_text':'Test target string'})
 
         self.assertRedirects(response, '/tms/{:d}/'.format(correct_tm.id))
-
-        
-
-
-
-class TransUnitandTMModelTest(TestCase):
-
-
-    def test_saving_and_retrieving(self):
-        tm = TM()
-        tm.save()
-
-
-        first_translation_unit = TransUnit()
-        first_translation_unit.source = "Source"
-        first_translation_unit.target = "Target"
-        first_translation_unit.tm = tm
-
-        first_translation_unit.save()
-
-        second_translation_unit = TransUnit()
-        second_translation_unit.source = 'The 2nd Source'
-        second_translation_unit.target = 'snd target'
-        second_translation_unit.tm = tm
-        second_translation_unit.save()
-
-        saved_tm = TM.objects.first()
-        self.assertEqual(saved_tm, tm)
-
-
-        saved_translation_units = TransUnit.objects.all()
-
-        self.assertEqual(saved_translation_units.count(), 2)
        
-        first_saved_translation_unit = saved_translation_units[0]
-        second_saved_translation_unit = saved_translation_units[1]
-
-        self.assertEqual(first_translation_unit, first_saved_translation_unit)
-        self.assertEqual(first_saved_translation_unit.tm, tm)
-        self.assertEqual(second_saved_translation_unit, second_translation_unit)
-
-        self.assertEqual(second_saved_translation_unit.tm, tm)
-
-        
